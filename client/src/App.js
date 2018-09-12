ccb9d9581c60599cd1624d694022d1aede50d6a4
Andrew <andrewbowden92@gmail.com>
2018-09-12 13:36:35 -0700

    FrontEnd

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro"> This works op </p>
-        <div id="box">Hold content.</div>
-        <img className="img-responsive" src={"https://www.smirnoff.com/en-us/wp-content/uploads/2015/02/WrappedInRubies-SmallGrid.jpg"} alt="logo"/>
+const App = (props) => (
+  <div>
+    <h1>My name is whattttt</h1>
+  </div>
+)
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro"> This works op </p>
+//         <div id="box">Hold content.</div>
+//         <img className="img-responsive" src={"https://www.smirnoff.com/en-us/wp-content/uploads/2015/02/WrappedInRubies-SmallGrid.jpg"} alt="logo"/>
 
 
-<section>
-  <p id="meh">Meh I am just annoyed.</p>
-</section>
-      </div>
-    );
+// <section>
+//   <p id="meh">Meh I am just annoyed.</p>
+// </section>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+//funnction that attaches state to prop
+//this state right here becomes your entire store.
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    name: state.name
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleName: dispatch({})
+  }
+}
+
+export default connect(null, null)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { combineReducers } from 'redux'
+import AddTodo from "./components/AddTodo";
+// import Visibility 
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
